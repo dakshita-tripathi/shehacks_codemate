6d09d54612b0dc45edffad7de1e5e139c2e3cd78
prakriti583 <97447737+prakriti583@users.noreply.github.com>
2022-03-12 11:24:33 +0530

    Create profile.dart

---
+++ lib/models/profile.dart
@@ -0,0 +1,11 @@
+class Profile {
+  final String name;
+  final String cc_rank;
+  final int he_rank;
+  final int apk_points;
+  Profile(
+      {required this.name,
+      required this.cc_rank,
+      required this.he_rank,
+      required this.apk_points});
+}
